9fb8496a804133e42d71ab258face510a7274d27
fengxiaojun <fengxiaojun@qq.com>
2017-03-28 16:48:58 +0800

    正在找dialog库

--- app/src/main/java/com/zzj/zuzhiji/LoginActivity.java
+++ app/src/main/java/com/zzj/zuzhiji/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.zzj.zuzhiji;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,9 +9,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.zzj.zuzhiji.app.Constant;
+import com.zzj.zuzhiji.network.ApiException;
+import com.zzj.zuzhiji.network.Network;
+import com.zzj.zuzhiji.util.DebugLog;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.Subscriber;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +28,10 @@ public class LoginActivity extends AppCompatActivity {
     EditText etVerify;
     @BindView(R.id.get_verify)
     TextView tvGetVerify;
+
+    private boolean isGetVerifyEnable = true;
+
+    CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,7 @@ public class LoginActivity extends AppCompatActivity {
     @OnClick(R.id.login)
     public void login(View view) {
         Toast.makeText(this, "登录", Toast.LENGTH_SHORT).show();
+
     }
 
     @OnClick(R.id.register)
@@ -42,13 +54,82 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.get_verify)
     public void getVerify(View view) {
-        if (TextUtils.isEmpty(etTel.getText().toString().trim())) {
+        if (!isGetVerifyEnable)
+            return;
+
+        if (TextUtils.isEmpty(etTel.getText().toString().trim()) || TextUtils.getTrimmedLength(etTel.getText().toString()) != 11) {
             Toast.makeText(this, "请输入正确的手机号", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        disableTvGetVerify();
+        startCountDownTime(Constant.COUNT_DOWN_TIME);
+
+        try {
+            Network.getInstance().sendSms(etTel.getText().toString().trim())
+                    .subscribe(new Subscriber<Object>() {
+                        @Override
+                        public void onCompleted() {
+
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onNext(Object o) {
+
+                        }
+                    });
+        } catch (ApiException ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 
+    private void startCountDownTime(long time) {
+        /**
+         * 最简单的倒计时类，实现了官方的CountDownTimer类（没有特殊要求的话可以使用）
+         * 即使退出activity，倒计时还能进行，因为是创建了后台的线程。
+         * 有onTick，onFinsh、cancel和start方法
+         */
+        timer = new CountDownTimer(time * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //每隔countDownInterval秒会回调一次onTick()方法
+                DebugLog.d("onTick  " + millisUntilFinished / 1000);
 
+                tvGetVerify.setText(String.format("%s秒", millisUntilFinished / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                DebugLog.d("onFinish -- 倒计时结束");
+                enableTvGetVerify();
+            }
+        };
+        timer.start();// 开始计时
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+    private void enableTvGetVerify() {
+        tvGetVerify.setTextColor(getResources().getColor(R.color.md_red_800));
+        tvGetVerify.setText("获取验证码");
+        isGetVerifyEnable = true;
+    }
+
+    private void disableTvGetVerify() {
+        tvGetVerify.setTextColor(getResources().getColor(R.color.md_red_800));
+        isGetVerifyEnable = false;
+    }
 }
--- app/src/main/java/com/zzj/zuzhiji/app/Constant.java
+++ app/src/main/java/com/zzj/zuzhiji/app/Constant.java
@@ -12,6 +12,9 @@ public interface Constant {
     }
 
     int DEFAULT_TIMEOUT = 5;
+
+    int COUNT_DOWN_TIME = 60;
+
     String BASE_URL_NOR = "http://101.201.155.115:6068";
     String BASE_URL_SMS = "http://101.201.155.115:8086";
 }
