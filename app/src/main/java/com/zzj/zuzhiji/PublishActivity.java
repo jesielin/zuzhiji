c1838949afc1cf76c23005fa8f073a0407e2b24e
fengxiaojun <fengxiaojun@qq.com>
2017-03-30 17:17:27 +0800

    发表朋友圈页面 更换photopicker photoreview photogrid

--- app/src/main/java/com/zzj/zuzhiji/PublishActivity.java
+++ app/src/main/java/com/zzj/zuzhiji/PublishActivity.java
@@ -1,8 +1,14 @@
 package com.zzj.zuzhiji;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,30 +20,55 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.jaeger.ninegridimageview.NineGridImageView;
 import com.jaeger.ninegridimageview.NineGridImageViewAdapter;
+import com.zzj.zuzhiji.app.Constant;
+import com.zzj.zuzhiji.network.Network;
 import com.zzj.zuzhiji.util.ActivityManager;
+import com.zzj.zuzhiji.util.DebugLog;
+import com.zzj.zuzhiji.util.GlideCircleTransform;
+import com.zzj.zuzhiji.util.SharedPreferencesUtils;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.bingoogolapple.photopicker.activity.BGAPhotoPickerActivity;
+import cn.bingoogolapple.photopicker.activity.BGAPhotoPickerPreviewActivity;
+import cn.bingoogolapple.photopicker.widget.BGANinePhotoLayout;
+import cn.bingoogolapple.photopicker.widget.BGASortableNinePhotoLayout;
+import droidninja.filepicker.FilePickerBuilder;
+import droidninja.filepicker.FilePickerConst;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 /**
  * Created by shawn on 2017-03-30.
  */
 
-public class PublishActivity extends AppCompatActivity {
+public class PublishActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks, BGASortableNinePhotoLayout.Delegate {
 
+    private static final int REQUEST_CODE_PERMISSION_PHOTO_PICKER = 1;
+    private static final int REQUEST_CODE_CHOOSE_PHOTO = 1;
+    private static final int REQUEST_CODE_PHOTO_PREVIEW = 2;
     @BindView(R.id.title)
     EditText etTitle;
     @BindView(R.id.subtitle)
     EditText etSubTitle;
+
+    /**
+     * 拖拽排序九宫格控件
+     */
     @BindView(R.id.image_group)
-    NineGridImageView nineGridImageView;
+    BGASortableNinePhotoLayout mPhotosSnpl;
 
-    private List<ImageModel> images = new ArrayList<>();
 
+    private ArrayList<String> paths = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,18 +81,52 @@ public class PublishActivity extends AppCompatActivity {
     }
 
     private void setupLayout() {
-        nineGridImageView.setAdapter(mImageAdapter);
-        images.add(new ImageModel(ImageModel.TYPE_ADD, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        images.add(0, new ImageModel(ImageModel.TYPE_IMAGE, R.drawable.add_pic));
-        nineGridImageView.setImagesData(images);
+        mPhotosSnpl.setMaxItemCount(9);
+        mPhotosSnpl.setEditable(true);
+        mPhotosSnpl.setPlusEnable(true);
+        mPhotosSnpl.setDelegate(this);
+        mPhotosSnpl.setData(paths);
+    }
+
+    @AfterPermissionGranted(REQUEST_CODE_PERMISSION_PHOTO_PICKER)
+    private void choicePhotoWrapper() {
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // 拍照后照片的存放目录，改成你自己拍照后要存放照片的目录。如果不传递该参数的话就没有拍照功能
+            File takePhotoDir = new File(Environment.getExternalStorageDirectory(), "BGAPhotoPickerTakePhoto");
+
+            startActivityForResult(BGAPhotoPickerActivity.newIntent(this, takePhotoDir, mPhotosSnpl.getMaxItemCount() - mPhotosSnpl.getItemCount(), null, false), REQUEST_CODE_CHOOSE_PHOTO);
+        } else {
+            EasyPermissions.requestPermissions(this, "图片选择需要以下权限:\n\n1.访问设备上的照片\n\n2.拍照", REQUEST_CODE_PERMISSION_PHOTO_PICKER, perms);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        if (requestCode == REQUEST_CODE_PERMISSION_PHOTO_PICKER) {
+            Toast.makeText(this, "您拒绝了「图片选择」所需要的相关权限!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_CHOOSE_PHOTO) {
+            mPhotosSnpl.addMoreData(BGAPhotoPickerActivity.getSelectedImages(data));
+
+        } else if (requestCode == REQUEST_CODE_PHOTO_PREVIEW) {
+            mPhotosSnpl.setData(BGAPhotoPickerPreviewActivity.getSelectedImages(data));
+        }
     }
 
     @OnClick(R.id.back)
@@ -76,52 +141,58 @@ public class PublishActivity extends AppCompatActivity {
             return;
         }
 
+        FilePickerBuilder
+                .getInstance()
+                .setMaxCount(9)
+                .setSelectedFiles(paths)
+                .setActivityTheme(R.style.AppTheme)
+                .pickPhoto(PublishActivity.this);
+
+
+//        MultipartBody.Part[] parts = new MultipartBody.Part[images.size()];
+//        Map<String, RequestBody> map = new ArrayMap<>();
+//        for (int i = 0; i < images.size(); i++) {
+//            File file = new File(String.valueOf(images.get(i).src));
+//            // 创建 RequestBody，用于封装构建RequestBody
+//            RequestBody requestFile =
+//                    RequestBody.create(MediaType.parse("multipart/form-data"), file);
+//
+//            // MultipartBody.Part  和后端约定好Key，这里的partName是用image
+//            parts[i] = MultipartBody.Part.createFormData("photos", file.getName(), requestFile);
+//            //map.put("photos"+i,)
+//
+//        }
+//
+//        //uuid
+//        String uuidText = SharedPreferencesUtils.getInstance().getValue(Constant.SHARED_KEY.UUID);
+//        RequestBody uuid =
+//                RequestBody.create(
+//                        MediaType.parse("multipart/form-data"), uuidText);
+//        // 添加message
+//        String messageText = etSubTitle.getText().toString();
+//        RequestBody message =
+//                RequestBody.create(
+//                        MediaType.parse("multipart/form-data"), messageText);
+//
+//
+//        Network.getInstance().postSocial(uuid, message, parts);
+
 
     }
 
-    private static class ImageModel {
-        public ImageModel(int type, Object src) {
-            this.type = type;
-            this.src = src;
-        }
 
-        public static int TYPE_ADD = 1;
-        public static int TYPE_IMAGE = 2;
-
-        public int type;
-        public Object src;
-
+    @Override
+    public void onClickAddNinePhotoItem(BGASortableNinePhotoLayout sortableNinePhotoLayout, View view, int position, ArrayList<String> models) {
+        choicePhotoWrapper();
     }
 
-    private NineGridImageViewAdapter<ImageModel> mImageAdapter = new NineGridImageViewAdapter<ImageModel>() {
-        @Override
-        protected void onDisplayImage(Context context, ImageView imageView, ImageModel bean) {
+    @Override
+    public void onClickDeleteNinePhotoItem(BGASortableNinePhotoLayout sortableNinePhotoLayout, View view, int position, String model, ArrayList<String> models) {
+        mPhotosSnpl.removeItem(position);
+    }
 
-            Glide.with(context).load(bean.src)
-                    .diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.color.md_blue_grey_400)
-                    .into(imageView);
-        }
-
-        @Override
-        protected ImageView generateImageView(Context context) {
-            return super.generateImageView(context);
-        }
-
-        @Override
-        protected void onItemImageClick(Context context, ImageView imageView, int index, List<ImageModel> list) {
-            super.onItemImageClick(context, imageView, index, list);
-//            Intent intent = new Intent(mContext,PhotoReviewActivity.class);
-//            intent.putExtra("position",index);
-//            intent.putExtra("list", list.toArray(new String[0]));
-//            context.startActivity(intent);
-            ImageModel bean = list.get(index);
-
-            if (bean.type == ImageModel.TYPE_ADD) {
-                Toast.makeText(context, index + ":添加图片", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(context, index + ":正常", Toast.LENGTH_SHORT).show();
-
-            }
-        }
-    };
+    @Override
+    public void onClickNinePhotoItem(BGASortableNinePhotoLayout sortableNinePhotoLayout, View view, int position, String model, ArrayList<String> models) {
+        startActivityForResult(BGAPhotoPickerPreviewActivity.newIntent(this, mPhotosSnpl.getMaxItemCount(), models, models, position, false), REQUEST_CODE_PHOTO_PREVIEW);
+    }
 }
